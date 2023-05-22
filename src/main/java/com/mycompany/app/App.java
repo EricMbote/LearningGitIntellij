ab4a3e171691fb0c6e542ee0ce0ab1ab5f3fcd3f
gamer12345me <134269643+gamer12345me@users.noreply.github.com>
2023-05-22 12:02:35 +0300

    Update App.java
    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!!!" );
     }
     //This code is written by Gamerme. Add 2 more exclamation marks
 
